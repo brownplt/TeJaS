95c40a9e0b76774f1139c1b4b485327e959e06b4
Jincheng Li <jincheng_li@brown.edu>
2012-09-11 17:58:41 -0400

    added file for examples from Learning JQuery 3rd Ed

---
+++ jQuery/src/LJExamples.js
@@ -0,0 +1,72 @@
+
+$(this);
+eq(7),get(0),slice; // get is childrenOf + eq?
+
+// Examples from Learning JQuery
+// Chapter 2
+// p41, Listing 2.10
+$(document).ready(function() {
+    $('td:contains(Henry)').next().addClass('highlight');
+});
+
+// p42, Listing 2.12
+$(document).ready(function() {
+    $('td:contains(Henry)').nextAll().andSelf()
+	.addClass('highlight');
+});
+
+
+// p43, Listing 2.13
+$(document).ready(function() {
+    $('td:contains(Henry)').parent().children()
+	.addClass('highlight');
+});
+
+
+// p43, Listing 2.15
+$('td:contains(Henry)')     // Find every cell containing "Henry"
+    .parent()               // Select its parent
+    .find('td:eq(1)')       // Find the 2nd descendant cell
+    .addClass('highlight')  // Add the "highlight" class
+    .end()              // Return to the parent of the cell containing "Henry"
+    .find('td:eq(2)')       // Find the 3rd descendant cell
+    .addClass('highlight'); // Add the "highlight" class
+
+
+// p229, Listing 9.2
+// Unfinished code
+$(document).ready(function() {
+    $('#topics a').click(function() {
+	var topic = $(this).text();
+	$('#topics a.selected').removeClass('selected');
+	$(this).addClass('selected');
+	$('#news tr').show();
+	if (topic != 'All') {
+	    $('#news tr:has(td):not(:contains("' + topic + '"))')
+                .hide();
+        }
+        return false;
+    });
+});
+
+
+// p230, Listing 9.3
+if (topic != 'All') {
+    $('#news').find('tr:has(td)').not(function() {
+        return $(this).children(':nth-child(4)').text() == topic;
+    }).hide();
+}
+
+
+// p246, Listing 9.15
+$(document).ready(function() {
+  function stripe() {
+    $('#news')
+      .find('tr.alt').removeClass('alt').end()
+      .find('tbody').each(function() {
+        $(this).children(':visible').has('td')
+          .filter(':group(3)').addClass('alt');
+    });
+  }
+  stripe();
+});
