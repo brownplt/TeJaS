f10ae5388c3d36a7622c9be80fc57f842efed0b4
Jincheng Li <jincheng_li@brown.edu>
2012-09-14 19:25:43 -0400

    updated examples and added some html elements to jq.env

--- jQuery/src/LJExamples.js
+++ jQuery/src/LJExamples.js
@@ -1,75 +1,122 @@
+/*::
+
+// Shakespeare's plays and sonnets (for examples 1 and 5)
+
+(Plays : h2
+         classes = [heading]);
+
+(PlaysTable : """Shakespeare's plays"""
+              table
+              classes = [plays]
+              (PlaysRow : tr
+                          classes = [playsrow]
+                          (PlayTitle : td classes = [title])
+                          (PlayGenre : td classes = [genre])
+                          (PlayYear : td classes = [year]))
+              <PlaysRow>);
+
+(Sonnets : h2
+           classes = [heading]);
+
+(SonnetsTable : table
+                classes = [sonnets]
+                (SonnetsRow : tr
+                               classes = [sonnetsrow] 
+                               (SonnetTitle : td
+                                              classes = [title, test])
+                               (SonnetLines : td
+                                              classes = [lines, test]))
+                <SonnetsRow>);
+
+// news (example 3)
+(NewsTable : table
+             classes = [news]
+             (NewsHeader : thead
+                           classes = [newsheader]
+                           (HeaderRow : tr
+                                        classes = [headerrow]
+                                        (HeaderCell : th
+                                                      classes = [headercell])
+                                        <HeaderCell>))
+             (NewsBody : tbody
+                         classes = [newsbody]
+                         (YearRow : tr
+                                    classes = [yearrow])
+                         (NewsRow : tr
+                                    classes = [newsrow]
+                                    (NewsDate : td
+                                                classes = [date])
+                                    (NewTitle : td
+                                                classes = [title])
+                                    (NewAuthor : td
+                                                 classes = [author])
+                                    (NewTopic : td
+                                                classes = [topic]))
+                         <NewsRow>
+                         <YearRow>
+                         <NewsRow>
+                         <NewsRow>))
+
+
+// sample div (example 4 and 5)
+(SampleDiv : div
+             ids = [jqdt2]
+             classes = [samplediv]
+             (Paragraph : p
+                          classes = [goofy])
+             (OrderedList : ol
+                            classes = [list]
+                            (LinkItem : li
+                                        classes = [linkitem]
+                                        (Link : a
+                                                classes = [link]))
+                            (GoofyItem : li
+                                        classes = [goofy])
+                            (FunnyItem : li
+                                        classes = [funny])
+                            <LinkItem>
+                            <GoofyItem>))
+
+
+*/
+
+
 // Examples from the book
 
 // Source:
 // Johnathan Chaffer, Karl Swedberg (2011), Learning JQuery (3rd ed.). Birmingham, UK: Packt Publishing Ltd.
 
 // p41-41, Listing 2.10
-$('td:contains(Henry)').next().addClass('highlight');
+// Example 1
+// simple selector, simple type with horizontal local structure traversal
+$('td').next().addClass('highlight');
 
 // p42-42, Listing 2.12
-$('td:contains(Henry)').nextAll().andSelf()
+// Example 5
+// simple selector, transitive traversal, jQuery stack
+$('td').nextAll().andSelf()
     .addClass('highlight');
 
 
-// p43-43, Listing 2.13
-$('td:contains(Henry)').parent().children()
-    .addClass('highlight');
-
-
-// p43-44, Listing 2.15
-$('td:contains(Henry)')     // Find every cell containing "Henry"
-    .parent()               // Select its parent
-    .find('td:eq(1)')       // Find the 2nd descendant cell
-    .addClass('highlight')  // Add the "highlight" class
-    .end()              // Return to the parent of the cell containing "Henry"
-    .find('td:eq(2)')       // Find the 3rd descendant cell
-    .addClass('highlight'); // Add the "highlight" class
-
-
-// p229-229, Listing 9.2
-$('#topics a').click(function() {
-    var topic = $(this).text();
-    $('#topics a.selected').removeClass('selected');
-    $(this).addClass('selected');
-    $('#news tr').show();
-    if (topic != 'All') {
-	$('#news tr:has(td):not(:contains("' + topic + '"))')
-            .hide();
-    }});
-
-
-// p230-230, Listing 9.3
-$('#news').find('tr:has(td)').not(function() {
-    return $(this).children(':nth-child(4)').text() == topic;
-}).hide();
-
-
-// p246-246, Listing 9.15
+p246-246, Listing 9.15
+Example 3
+assume each does not modify local structure
 $('#news')
-    .find('tr.alt').removeClass('alt').end()
+    .find('tr.alt')// .removeClass('alt')
+    .end()
     .find('tbody').each(function() {
-        $(this).children(':visible').has('td')
-            .filter(':group(3)').addClass('alt');
+        // $(this).children(':visible').has('td')
+        //     .filter(':group(3)').addClass('alt');
     });
 
 
 // Examples from blog of Learning JQuery
 
 // Source:
-// Karl Swedberg (2006), How to Get Anything You Want - part 1
-// http://www.learningjquery.com/2006/11/how-to-get-anything-you-want-part-1
-
-$('li').eq(0);
-$('li').not('.goofy');
-$('ol .goofy > strong');
-$('li + li > a[href$=pdf]');
-$('span:hidden');
-
-// Source:
 // Karl Swedberg (2006), How To Get Anything You Want - part 2
 // http://www.learningjquery.com/2006/12/how-to-get-anything-you-want-part-2
 
-$('#jqdt2').find('li').siblings();
+// Example 2
 $('#jqdt2')
     .find('li.funny')
     .css('backgroundColor', '#0ff')
@@ -77,23 +124,88 @@ $('#jqdt2')
     .find('p > strong')
     .css('backgroundColor', '#ff0');
 
-
+// Example 4
 $('li.goofy')
-    .parents('#jqdt2')
-    .children('p')
+    // .parents('#jqdt2')
+    .parents()
+    .filter('#jqdt2')
+    // .children('p')
+    .children()
+    .filter('p')
     .next()
-    .find('a')
+    // .find('a')
+    .find()
+    .filter('a')
     .parent();
 
 
 
+// End selected examples
 
 
 
-// Only example of add() in the book, not very friendly to the type checker
-var colNum = $td[0].cellIndex + 1;
-var $columnCells = $td
-.closest('table')
-.find('td, th')
-.filter(':nth-child(' + colNum + ')');
-$cells = $cells.add($columnCells);
+
+
+// // Source:
+// // Karl Swedberg (2006), How to Get Anything You Want - part 1
+// // http://www.learningjquery.com/2006/11/how-to-get-anything-you-want-part-1
+
+// $('li').eq(0);
+// $('li').not('.goofy');
+
+// // selector with structural information
+// $('ol .goofy > strong');
+
+// // cannot deal with attributes in selectors
+// $('li + li > a[href$=pdf]');
+// $('span:hidden');
+
+
+// // HTGAYW2
+// // transitive
+// $('#jqdt2').find('li').siblings();
+
+// // End HTGAYW
+
+
+// // p43-44, Listing 2.15
+// // chaining, stack
+// $('td')     // Find every cell containing "Henry"
+//     .parent()               // Select its parent
+//     .find('td:eq(1)')       // Find the 2nd descendant cell
+//     .addClass('highlight')  // Add the "highlight" class
+//     .end()              // Return to the parent of the cell containing "Henry"
+//     .find('td:eq(2)')       // Find the 3rd descendant cell
+//     .addClass('highlight'); // Add the "highlight" class
+
+
+
+// // p43-43, Listing 2.13
+// // simple selector, vertical traversal
+// $('td').parent().children()
+//     .addClass('highlight');
+
+
+// // p229-229, Listing 9.2
+// // selector with structural information
+// $('#topics a.selected').removeClass('selected');
+// $('#news tr').show();
+
+
+
+// // p230-230, Listing 9.3
+// // not interesting for our purposes (no support for not() or user-defined function)
+// $('#news').find('tr:has(td)').not(function() {
+//     return $(this).children(':nth-child(4)').text() == topic;
+// }).hide();
+
+
+
+// // Only example of add() in the book, not very friendly to the type checker
+// var colNum = $td[0].cellIndex + 1;
+// var $columnCells = $td
+// .closest('table')
+// .find('td, th')
+// .filter(':nth-child(' + colNum + ')');
+// $cells = $cells.add($columnCells);
+
